404dc5dfee28b67bb3e2285355e29de65f78b878
Frederik P. <frepbh@gmail.com>
2024-04-24 20:15:30 +0200

    made the stack stack based lol

--- src/stack.rs
+++ src/stack.rs
@@ -1,53 +1,52 @@
-use std::{fmt::Debug, ptr::null_mut};
-
-use crate::memory::reallocate;
+use std::{fmt::Debug, mem::MaybeUninit};
 
 pub struct Stack<T, const STACK_SIZE: usize = 1024> {
-    stack: *mut T,
-    stack_top: *mut T,
+    stack: [MaybeUninit<T>; STACK_SIZE], // Be careful
+    count: usize,
 }
 
-impl<T, const STACK_SIZE: usize> Stack<T, STACK_SIZE> {
+impl<'a, T, const STACK_SIZE: usize> Stack<T, STACK_SIZE> {
+    const ARRAY_REPEAT_VALUE: MaybeUninit<T> = MaybeUninit::uninit();
+
     pub fn new() -> Self {
-        let stack = reallocate::<T>(null_mut(), 0, STACK_SIZE).cast();
         Self {
-            stack,
-            stack_top: stack,
+            stack: [Self::ARRAY_REPEAT_VALUE; STACK_SIZE],
+            count: 0,
         }
     }
 
-    pub const fn count(&self) -> isize {
-        unsafe { self.stack_top.offset_from(self.stack) }
-    }
-
-    pub fn get_top_mut_ref(&mut self) -> &mut T {
-        unsafe { self.stack_top.sub(1).as_mut().unwrap() }
+    pub const fn count(&self) -> usize {
+        self.count
     }
 
     pub fn push(&mut self, val: T) {
-        //TODO: consider making the stack resizable and check for cap/size here.
-        unsafe {
-            self.stack_top.write(val);
-            self.stack_top = self.stack_top.add(1);
-        }
+        self.stack[self.count].write(val);
+        self.count += 1;
+    }
+
+    pub fn get_top_mut_ref(&mut self) -> &mut T {
+        unsafe { self.stack[self.count - 1].assume_init_mut() }
     }
 
     pub fn pop(&mut self) -> T {
-        //TODO: consider making the stack resizable and check for cap/size here.
         unsafe {
-            self.stack_top = self.stack_top.sub(1);
-            self.stack_top.read()
+            let val = self.stack[self.count - 1].assume_init_read();
+            self.count -= 1;
+            val
         }
     }
 
-    pub fn peek(&self, distance: usize) -> &T {
-        unsafe { &*self.stack_top.sub(distance + 1) }
+    pub const fn peek(&self, distance: usize) -> &T {
+        unsafe { self.stack[self.count - distance].assume_init_ref() }
     }
 }
 
 impl<T, const STACK_SIZE: usize> Drop for Stack<T, STACK_SIZE> {
     fn drop(&mut self) {
-        reallocate::<T>(self.stack.cast(), STACK_SIZE, 0);
+        for i in 0..self.count {
+            unsafe { self.stack[i].assume_init_drop() };
+        }
+        self.count = 0;
     }
 }
 
@@ -56,23 +55,12 @@ where
     T: Debug,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let count = self.count();
-        if count < 1 {
-            return Ok(());
-        }
-        let mut ptr = self.stack;
         f.write_str("          ")?;
-        loop {
-            if ptr >= self.stack_top {
-                break;
-            }
-            unsafe {
-                f.write_str("[ ")?;
-                let val = ptr.read();
-                f.write_fmt(format_args!("{:?}", val))?;
-                f.write_str(" ]")?;
-                ptr = ptr.add(1);
-            }
+        for i in 0..self.count {
+            let elem = unsafe { self.stack[i].assume_init_ref() };
+            f.write_str("[ ")?;
+            f.write_fmt(format_args!("{:?}", elem))?;
+            f.write_str(" ]")?;
         }
         f.write_str("\n")?;
         Ok(())
